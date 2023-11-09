ef2568f4e995a072231d177dbfb2754ec874a2b3
kdongk <ldh5081@naver.com>
2023-11-09 16:58:55 +0900

    fix: csv파일 컬럼 단위로 조회하는 방식으로 수정

--- chatKAU_main/views.py
+++ chatKAU_main/views.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import chromadb
+from googletrans import Translator
+from chromadb.utils import embedding_functions
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import JsonResponse
@@ -6,103 +10,110 @@ from django.views.decorators.http import require_POST
 import json
 import os
 import openai
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings.openai import OpenAIEmbeddings 
 from rest_framework.decorators import api_view
-
 from chatKAU_main.models import SchoolInfo
-
-vectordb = None
 
 os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# def initialize_vectordb():
-#     global vectordb
-    
-#     loader = CSVLoader("./test3_filter.csv")
-#     documents = loader.load()
-    
-#     text_splitter = RecursiveCharacterTextSplitter(
-#         chunk_size = 1500,
-#         chunk_overlap = 150
-#     )
-#     splits = text_splitter.split_documents(documents)
-    
-#     persist_directory = 'docs/chroma/'
-#     embedding = OpenAIEmbeddings(
-#         model= "text-embedding-ada-002"
-#     )
-    
-#     vectordb = Chroma.from_documents(
-#         documents=splits,
-#         embedding=embedding,
-#         persist_directory=persist_directory
-#     )
+collection = None
+ids = []
+documents = []
+doc_meta = []
 
-# if vectordb is None:
-#     initialize_vectordb()
+def index(request):
+    return render(request, 'home.html')
 
+
+def translate_to_english(text):
+    translator = Translator()
+    translated = translator.translate(text, src='auto', dest='en')
+    return translated.text
+
+
+def initialize_vectordb():
+    global collection
+    if collection is not None:
+        return
+    
+    df = pd.read_csv("./kau_data_eng.csv")
+
+    client = chromadb.PersistentClient(path="docs/chroma/")
+    
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=openai.api_key,
+                model_name="text-embedding-ada-002"
+    )
+    
+    collection = client.get_or_create_collection(
+        name="schoolInfo",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    
+    for i in range(len(df)):
+        item = df.iloc[i]
+        
+        meta = {
+            "row": str(item['INDEX']),
+            "source": item['URL']
+        }
+        
+        doc_meta.append(meta)
+        ids.append(item['CONTENT'])
+        documents.append(item['ENGLISH'])
+        
+    collection.add(
+        documents=documents,
+        metadatas=doc_meta,
+        ids=ids
+    )
+    
 
 @csrf_exempt
 @api_view(['POST'])  
 def langchain(request):
-    global vectordb
+    data = json.loads(request.body.decode("utf-8"))
+    question = data["messages"][0]["content"]
     
-    if request.method == "OPTIONS":
-        response = JsonResponse({})
-        response["Access-Control-Allow-Origin"] = "*" 
-        response["Access-Control-Allow-Methods"] = "POST, OPTIONS"  
-        response["Access-Control-Allow-Headers"] = "origin, content-type, accept" 
-        return response
-
-    elif request.method == "POST":
-        data = json.loads(request.body.decode("utf-8"))
-        question = data["messages"][0]["content"]
+    english_question = translate_to_english(question)
+    print(english_question)
+    
+    initialize_vectordb()
+    
+    queryset = SchoolInfo.objects.filter(keyword=question)
+    
+    if queryset.exists():
+        gpt_response = queryset.first().content
+        content_origin = 'DB'
+        metadata = 'DB'
+    
+    else:
+        result = collection.query(
+            query_texts=english_question,
+            n_results=2
+        )
         
-        queryset = SchoolInfo.objects.filter(keyword=question)
+        content_origin = result['ids'][0][0]      
+        metadata = result['metadatas'][0][0]
         
-        if queryset.exists():
-            gpt_response = queryset.first().content
-            response_content = 'DB'
-            metadata = 'DB'
-        else:
-            docs = vectordb.similarity_search(question, k=3)
-            response_content = docs[0].page_content
-            
-            print(docs)
-            
-            metadata = docs[0].metadata
-            
-            response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[{
-                "role": "user",
-                "content": (
-                    f"이건 항공대학교에 대한 정보야. "
-                    f"내가 준 기반 정보를 바탕으로만 대답해. "
-                    f"기반 정보: {response_content} / "
-                    f"내 질문: {question}"
-                )
-            }])
-            gpt_response = response['choices'][0]['message']['content']
-        
-        
-        return JsonResponse({
-            "choices": [{
-                "message": {
-                    "content": gpt_response,
-                    "content_origin": response_content,
-                    "metadata": metadata
-                }
-            }]
-        })
-
-    return JsonResponse({"error": "Only POST requests are allowed."}, status=400)
-
-
-def index(request):
-    return render(request, 'home.html')
+        response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": 
+                """너는 항공대학교에 대한 정보를 간략하고 꼼꼼하게 설명해주는 챗봇이야. 
+                기반정보를 이용해서만 대답해. 기반정보에 없는 내용은 답변에 포함하지마."""},
+            {"role": "assistant", "content": f"기반정보: {content_origin}"},
+            {"role": "user", "content": question}
+        ])
+        gpt_response = response['choices'][0]['message']['content']
+    
+    return JsonResponse({
+        "choices": [{
+            "message": {
+                "content": gpt_response,
+                "content_origin": content_origin,
+                "metadata": metadata
+            }
+        }]
+    })
